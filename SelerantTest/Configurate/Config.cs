b5f24a45167535d15d7e62910e685c861d980a15
Михаил <qo_opmisha@live.ru>
2020-01-02 02:21:08 +0200

    add config

---
+++ SelerantTest/Configurate/Config.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SelerantTest.Configurate
+{
+    internal class Config
+    {
+        #region ctor and fields
+        private Dictionary<string, Config> PairKeyValues = new Dictionary<string, Config>();
+        private string _value;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private Config() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        internal Config(string path)
+        {
+            using (var file = File.Open(path, FileMode.Open))
+            {
+                using (var stream = new StreamReader(file))
+                {
+                    while (stream.Peek() >= 0)
+                    {
+                        var line = stream.ReadLine();
+                        var splitValue = line.Split('=');
+
+                        if (splitValue.Length != 2) continue;
+
+                        var key = splitValue[0];
+                        var val = splitValue[1];
+                        AddKeyValue(key, val);
+                    }
+                }
+            }
+        }
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return _value;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        internal Config this[string key]
+        {
+            get
+            {
+                if (!PairKeyValues.ContainsKey(key))
+                    return new Config();
+                return PairKeyValues[key];
+            }
+            set { PairKeyValues[key] = value; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        private void AddKeyValue(string key, string value)
+        {
+            var splitData = key.Split('.');
+            Config currentConfig = this;
+
+            for (var i = 0; i < splitData.Length; i++)
+            {
+                var val = splitData[i];
+                if (currentConfig.PairKeyValues.ContainsKey(val))
+                    currentConfig = currentConfig.PairKeyValues[val];
+                else
+                {
+
+                    var temp = new Config();
+                    currentConfig.PairKeyValues.Add(val, temp);
+                    currentConfig = temp;
+                }
+            }
+            currentConfig._value = value;
+        }
+    }
+}
--- SelerantTest/Program.cs
+++ SelerantTest/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
+using System.IO;
+using SelerantTest.Configurate;
 
 namespace SelerantTest
 {
     class Program
     {
+
+        private const string CONFIG_FILE_NAME = "Config.txt";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ConfigTask();
+
+            Console.ReadLine();
+        }
+        private static void ConfigTask()
+        {
+            string filePath = Path.Combine(Environment.CurrentDirectory, CONFIG_FILE_NAME);
+            var config = new Config(filePath);
+
+            Console.WriteLine(config["id"]);
+            Console.WriteLine(config["session"]["timeout"]);
+            Console.WriteLine(config["session"]["server"]["0"]["host"]);
+            Console.WriteLine(config["session"]["server"]["0"]["port"]);
+            Console.WriteLine(config["session"]["server"]["0"]["id"]);
+            Console.WriteLine(config["session"]["server"]["1"]["host"]);
+            Console.WriteLine(config["session"]["server"]["1"]["port"]);
+            Console.WriteLine(config["session"]["server"]["1"]["id"]);
+            Console.WriteLine(config["image"]["cat"]["width"]);
+            Console.WriteLine(config["image"]["cat"]["height"]);
+            Console.WriteLine(config["image"]["folder"]);
+            Console.WriteLine(config["test1"]["data"]);
+
         }
     }
 }
